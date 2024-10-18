53bd3139f18fb0e9a2fbf7aeae1656db71ec9a08
NamDinh2100 <148228189+NamDinh2100@users.noreply.github.com>
2024-10-18 10:31:58 +0700

    Update Controller.h

--- TowerDefense/Controller.h
+++ TowerDefense/Controller.h
@@ -9,6 +9,23 @@
 
 using namespace std;
 
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define AQUA 3
+#define RED 4
+#define PURPLE 5
+#define YELLOW 6
+#define WHITE 7
+#define GRAY 8
+//#define LIGHT_BLUE 9
+//#define LIGHT_GREEN 10
+//#define LIGHT_AQUA 11
+//#define LIGHT_RED 12
+//#define LIGHT_BLUE 13
+//#define LIGHT YELLOW 14
+//#define BRIGHT_WHITE 15
+
 class Controller {
 public:
     static HWND consoleWindow;
