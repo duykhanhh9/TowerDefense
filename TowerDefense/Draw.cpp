cfebd75eb12faca6c533fa14b1dc2af8f5e05a19
duykhanh0903 <165666289+duykhanhh9@users.noreply.github.com>
2024-10-25 14:30:45 +0700

    Update Draw.cpp

--- TowerDefense/Draw.cpp
+++ TowerDefense/Draw.cpp
@@ -490,3 +490,152 @@ void Draw::drawMonster(SHORT x, SHORT y, SHORT width, SHORT height)
     inFile.close();
 }
 
+////////////25/10
+void Draw::drawIcon(SHORT x, SHORT y, SHORT width, SHORT height, int color, int background) {
+    for (int i = 0; i < width - 2; i++) {
+        createColorBlock(x + i, y, color, color, " ");
+        createColorBlock(x + i, y + height, color, color, " ");
+    }
+    
+    for (int i = 0; i < width + 2; i++) {
+        if (x + i - 2 < x + 2 || x + i - 2 >= x + width - 4) {
+            createColorBlock(x + i - 2, y + 1, color, color, " ");
+            createColorBlock(x + i - 2, y + height - 1, color, color, " ");
+        }
+        else {
+            createColorBlock(x + i - 2, y + 1, background, background, " ");
+            createColorBlock(x + i - 2, y + height - 1, background, background, " ");
+        }
+    }
+
+    for (int i = 0; i < width + 6; i++) {
+        if (x + i - 4 < x || x + i - 4 >= x + width - 2) {
+            createColorBlock(x + i - 4, y + 2, color, color, " ");
+            createColorBlock(x + i - 4, y + height - 2, color, color, " ");
+        }
+        else {
+            createColorBlock(x + i - 4, y + 2, background, background, " ");
+            createColorBlock(x + i - 4, y + height - 2, background, background, " ");
+        }
+    }
+
+    for (int i = 0; i < height - 5; i++) {
+        createColorBlock(x - 4, y + 3 + i, color, color, "  ");
+        drawBlock(x - 2, y + 3 + i, width + 2, 1, background);
+        createColorBlock(x + width, y + 3 + i, color, color, "  ");
+    }
+}
+
+// playIcon
+void Draw::drawIconPlay(SHORT x, SHORT y, SHORT width, SHORT height, int color) {
+
+    ifstream ifs("playIcon.txt");
+    int row, column, tmp;
+    if (ifs.is_open()) {
+        ifs >> row;
+        ifs >> column;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                ifs >> tmp;
+                if (tmp == 1) {
+                    drawBlock(x + j * width + 20, y + i * height + 7, width, height, color);
+                }
+            }
+        }
+    }
+
+    ifs.close();
+}
+
+void Draw::drawIconLeaderBoard(SHORT x, SHORT y, SHORT width, SHORT height, int color) {
+    ifstream ifs("leaderBoard.txt");
+    int row, column, tmp;
+    if (ifs.is_open()) {
+        ifs >> row;
+        ifs >> column;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                ifs >> tmp;
+                if (tmp == 1) {
+                    drawBlock(x + j * width + 2, y + i * height + 5, width, height, color);
+                }
+            }
+        }
+    }
+
+    ifs.close();
+}
+
+void Draw::drawIconExit(SHORT x, SHORT y, SHORT width, SHORT height, int color) {
+    ifstream ifs("exit.txt");
+    int row, column, tmp;
+    if (ifs.is_open()) {
+        ifs >> row;
+        ifs >> column;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                ifs >> tmp;
+                if (tmp == 1) {
+                    drawBlock(x + j * width + 8, y + i * height + 7, width, height, color);
+                }
+            }
+        }
+    }
+
+    ifs.close();
+}
+
+void Draw::drawIconSave(SHORT x, SHORT y, SHORT width, SHORT height, int color) {
+    ifstream ifs("save.txt");
+    int row, column, tmp;
+    if (ifs.is_open()) {
+        ifs >> row;
+        ifs >> column;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                ifs >> tmp;
+                if (tmp == 1) {
+                    drawBlock(x + j * width + 8, y + i * height + 5, width, height, color);
+                }
+            }
+        }
+    }
+
+    ifs.close();
+}
+
+void Draw::drawIconMusic(SHORT x, SHORT y, SHORT width, SHORT height, int color) {
+    ifstream ifs("music.txt");
+    int row, column, tmp;
+    if (ifs.is_open()) {
+        ifs >> row;
+        ifs >> column;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                ifs >> tmp;
+                if (tmp == 1) {
+                    drawBlock(x + j * width, y + i * height + 3, width, height, color);
+                }
+            }
+        }
+    }
+    ifs.close();
+}
+
+void Draw::drawIconHelp(SHORT x, SHORT y, SHORT width, SHORT height, int color) {
+    ifstream ifs("help.txt");
+    int row, column, tmp;
+    if (ifs.is_open()) {
+        ifs >> row;
+        ifs >> column;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                ifs >> tmp;
+                if (tmp == 1) {
+                    drawBlock(x + j * width + 4, y + i * height + 3, width, height, color);
+                }
+            }
+        }
+    }
+    ifs.close();
+}
