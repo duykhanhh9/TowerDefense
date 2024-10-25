85e6119beca26074b9a9bbd39331aab9cc7e7ad9
NamDinh2100 <148228189+NamDinh2100@users.noreply.github.com>
2024-10-25 10:12:28 +0700

    Update Draw.h

--- TowerDefense/Draw.h
+++ TowerDefense/Draw.h
@@ -6,39 +6,50 @@ using namespace std;
 class Draw
 {
 	public:
-          static void drawBlock(int, int, SHORT, SHORT, int);
+        static void drawBlock(SHORT, SHORT, SHORT, SHORT, int);
 
-          static void drawA(int, int, SHORT, SHORT, int);
-          static void drawB(int, int, SHORT, SHORT, int);
-          static void drawC(int, int, SHORT, SHORT, int);
-          static void drawD(int, int, SHORT, SHORT, int);
-          static void drawE(int, int, SHORT, SHORT, int);
-          static void drawF(int, int, SHORT, SHORT, int);
-          static void drawG(int, int, SHORT, SHORT, int);
-          static void drawH(int, int, SHORT, SHORT, int);
-          static void drawI(int, int, SHORT, SHORT, int);
-          static void drawJ(int, int, SHORT, SHORT, int);
-          static void drawK(int, int, SHORT, SHORT, int);
-          static void drawL(int, int, SHORT, SHORT, int);
-          static void drawM(int, int, SHORT, SHORT, int);
-          static void drawN(int, int, SHORT, SHORT, int);
-          static void drawO(int, int, SHORT, SHORT, int);
-          static void drawP(int, int, SHORT, SHORT, int);
-          static void drawQ(int, int, SHORT, SHORT, int);
-          static void drawR(int, int, SHORT, SHORT, int);
-          static void drawS(int, int, SHORT, SHORT, int);
-          static void drawT(int, int, SHORT, SHORT, int);
-          static void drawU(int, int, SHORT, SHORT, int);
-          static void drawV(int, int, SHORT, SHORT, int);
-          static void drawW(int, int, SHORT, SHORT, int);
-          static void drawX(int, int, SHORT, SHORT, int);
-          static void drawY(int, int, SHORT, SHORT, int);
-          static void drawZ(int, int, SHORT, SHORT, int);
+        static void drawA(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawB(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawC(SHORT, SHORT, SHORT, SHORT, int);
 
-          static void drawBox(int, int, SHORT, SHORT, int);
+        static void drawD(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawE(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawF(SHORT, SHORT, SHORT, SHORT, int);
 
-                static void turnSound(int, int, int, int);
-                static void drawLeft(int, int, int, SHORT, SHORT);
-                static void drawRight(int, int, int, SHORT, SHORT);
+        static void drawG(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawH(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawI(SHORT, SHORT, SHORT, SHORT, int);
 
+        static void drawJ(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawK(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawL(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawM(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawN(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawO(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawP(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawQ(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawR(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawS(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawT(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawU(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawV(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawW(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawX(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawY(SHORT, SHORT, SHORT, SHORT, int);
+        static void drawZ(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawBox(SHORT, SHORT, SHORT, SHORT, int);
+
+        static void drawSettingIcon(SHORT, SHORT, SHORT, SHORT);
+        static void drawHeartIcon(SHORT, SHORT, SHORT, SHORT);
+        static void drawCoinIcon(SHORT, SHORT, SHORT, SHORT);
+        static void drawPineIcon(SHORT, SHORT, SHORT, SHORT);
+
+
+        static void drawSantaClaus(SHORT, SHORT, SHORT, SHORT);
 };
